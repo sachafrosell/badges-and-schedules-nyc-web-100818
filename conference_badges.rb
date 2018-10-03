35ae362cf07cb4db679c04657a71ec5bb5c917c7
Alexander Frosell <sachafrosell@gmail.com>
2018-10-03 23:31:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 def assign_rooms(array)
   new_array = []
   array.each_with_index do |name, index|
-    new_array.push("Hello, #{name}! You'll be assigned to room #{index + 1}")
+    new_array.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
   end
   return new_array
 end
