f367e55f2fec961acdd59bbd1b591476afa2ea7b
Alexander Frosell <sachafrosell@gmail.com>
2018-10-03 23:35:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,8 +17,10 @@ def assign_rooms(array)
 end
 
 def printer
-  array.each do |badge|
+  batch_badge_creator(array).each do |badge|
     puts badge
   end
-  
+  assign_rooms(array).each do |room|
+    puts room
+  end
 end
